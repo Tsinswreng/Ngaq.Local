a00b040cb0a8b1f1d91b04ebd0859a75edc7b153
Tsinswreng <Tsinswreng@gmail.com>
2025-06-12 11:23:43 +0800

    遷依賴注入配置

---
+++ DiLocal.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data;
+using Ngaq.Core.Infra.Db;
+using Ngaq.Core.Model.Po.Kv;
+using Ngaq.Core.Model.Po.Learn_;
+using Ngaq.Core.Model.Po.Word;
+using Ngaq.Core.Model.UserCtx;
+using Ngaq.Core.Service.Word;
+
+//using Microsoft.CodeAnalysis.Text;
+using Ngaq.Core.Tools;
+using Ngaq.Core.Word;
+using Ngaq.Core.Word.Svc;
+using Ngaq.Db;
+using Ngaq.Local.Dao;
+using Ngaq.Local.Db;
+using Ngaq.Local.Service.Word;
+using Ngaq.Local.Word.Svc;
+using Tsinswreng.CsSqlHelper;
+using Tsinswreng.CsSqlHelper.Cmd;
+
+
+namespace Ngaq.Local;
+
+
+public class DiLocal{
+
+	public static IServiceCollection SetUpLocal(IServiceCollection z){
+
+
+z.AddDbContext<LocalDbCtx>();
+//svc.AddScoped<DaoWord, DaoWord>();
+z.AddScoped<DaoSqlWord, DaoSqlWord>();
+z.AddScoped<ISqlCmdMkr, SqlCmdMkr>();
+z.AddSingleton<IDbConnection>(AppTblInfo.Inst.DbConnection);
+z.AddTransient<MgrLearn, MgrLearn>();
+z.AddSingleton<ITableMgr>(AppTableMgr.Inst);
+z.AddScoped<RepoSql<PoWord,	IdWord>>();
+z.AddScoped<RepoSql<PoKv,	IdKv>>();
+z.AddScoped<RepoSql<PoLearn,	IdLearn>>();z.AddScoped<IRunInTxn, SqlTxnRunner>();
+z.AddScoped<ITxnRunner, SqlTxnRunner>();
+z.AddScoped<ISvcParseWordList, SvcParseWordList>();
+z.AddScoped<ISvcWord, SvcWord>();
+z.AddScoped<IWeightCalctr, SvcWeight>();
+z.AddScoped<IUserCtxMgr, UserCtxMgr>();
+z.AddScoped<IGetTxn, SqlCmdMkr>();
+return z;
+
+	}
+
+}
---
+++ ImplFrontend/SvcImg.cs
@@ -0,0 +1,12 @@
+using Ngaq.Core.FrontendIF;
+using Tsinswreng.CsCore.Tools;
+
+namespace Ngaq.Local.ImplFrontend;
+
+// public class SvcImg:IImgGetter{
+
+// 	public ITypedObj GetN(u64 n){
+
+// 	}
+
+// }
