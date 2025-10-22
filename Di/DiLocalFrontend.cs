92b50bd4b508096a411a1ddc61181f4c8485abea
Tsinswreng <Tsinswreng@gmail.com>
2025-10-22 21:08:46 +0800

    製 SvcTokenStorage;

--- Di/DiLocalFrontend.cs
+++ Di/DiLocalFrontend.cs
@@ -1,12 +1,15 @@
 namespace Ngaq.Local.Di;
 
 using Microsoft.Extensions.DependencyInjection;
+using Ngaq.Core.Frontend.User.Svc;
 using Ngaq.Core.Shared.Encryption.Svc;
 using Ngaq.Local.Domains.Encryption;
+using Ngaq.Local.Frontend.User.Svc;
 
 public static class DiLocalFrontend{
 	public static IServiceCollection SetupLocalFrontend(this IServiceCollection z){
 		z.AddScoped<ISvcSecretKv, SvcSecretKv>();
+		z.AddScoped<ISvcTokenStorage, SvcTokenStorage>();
 		return z;
 	}
 }
---
+++ Frontend/User/Svc/SvcTokenStorage.cs
@@ -0,0 +1,42 @@
+namespace Ngaq.Local.Frontend.User.Svc;
+
+using Ngaq.Core.Frontend.Kv;
+using Ngaq.Core.Frontend.User.Svc;
+using Ngaq.Core.Shared.Kv.Models;
+using Ngaq.Core.Shared.Kv.Svc;
+using Ngaq.Core.Shared.User.Models.Po.User;
+using Ngaq.Core.Shared.Word.Models.Po.Kv;
+using Tsinswreng.CsCore;
+
+public class SvcTokenStorage:ISvcTokenStorage{
+	//public ISvcSecretKv SvcSecretKv;
+	public ISvcKv SvcKv;
+	public SvcTokenStorage(
+		//ISvcSecretKv SvcSecretKv
+		ISvcKv SvcKv
+	){
+		//this.SvcSecretKv = SvcSecretKv;
+		this.SvcKv = SvcKv;
+	}
+
+	[Impl]
+	public async Task<str?> GetRefreshToken(CT Ct){
+		//TODO 先直接存明文 後汶改加密
+		var kv = await SvcKv.GetByOwnerEtKeyAsy(
+			IdUser.Zero, KeysClientKv.RefreshTokenCipher, Ct
+		);
+		return kv?.GetVStr();
+	}
+
+	[Impl]
+	public async Task<nil> SetRefreshToken(str Token, CT Ct){
+		//TODO 先直接存明文 後汶改加密
+		await SvcKv.SetAsy(
+			new PoKv{
+				Owner = IdUser.Zero,
+			}.SetStr(KeysClientKv.RefreshTokenCipher, Token)
+			,Ct
+		);
+		return NIL;
+	}
+}
