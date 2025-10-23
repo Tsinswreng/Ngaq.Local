b3ba07dfea3f67ad101a298d71294609a6266577
Tsinswreng <Tsinswreng@gmail.com>
2025-10-23 20:48:25 +0800

    Update DiLocalFrontend.cs

--- Di/DiLocalFrontend.cs
+++ Di/DiLocalFrontend.cs
@@ -5,6 +5,7 @@ using Ngaq.Core.Frontend.User;
 using Ngaq.Core.Frontend.User.Svc;
 using Ngaq.Core.Infra.Url;
 using Ngaq.Core.Shared.Encryption.Svc;
+using Ngaq.Core.Shared.User.UserCtx;
 using Ngaq.Local.Domains.Encryption;
 using Ngaq.Local.Frontend.User.Svc;
 using Ngaq.Local.Infra;
@@ -14,7 +15,9 @@ public static class DiLocalFrontend{
 		z.AddScoped<ISvcSecretKv, SvcSecretKv>();
 		z.AddScoped<ISvcTokenStorage, SvcTokenStorage>();
 		z.AddSingleton<IFrontendUserCtxMgr>(FrontendUserCtxMgr.Inst);
+		z.AddSingleton<IUserCtx>(FrontendUserCtxMgr.Inst.GetUserCtx());
 		z.AddSingleton<I_GetBaseUrl, BaseUrl>();
+
 		return z;
 	}
 }
