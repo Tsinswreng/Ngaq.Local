8cbd85ba606f5f33222d7a7f277e553bba407a5c
Tsinswreng <Tsinswreng@gmail.com>
2026-04-27 15:48:44 +0800

    SvcTokenStorage除蠹

--- Frontend/User/Svc/SvcTokenStorage.cs
+++ Frontend/User/Svc/SvcTokenStorage.cs
@@ -13,6 +13,22 @@ using Tsinswreng.CsTools;
 public class SvcTokenStorage:ISvcTokenStorage{
 	//public ISvcSecretKv SvcSecretKv;
 	public ISvcKv SvcKv;
+
+	/// <summary>
+	/// 先查舊行再沿用 Id 寫回，避免同一組客戶端鍵被當成新行插入。
+	/// </summary>
+	/// <param name="Key">客戶端鍵名。</param>
+	/// <param name="Kv">準備寫回的資料列。</param>
+	/// <param name="Ct">取消令牌。</param>
+	/// <returns>可直接交給 <see cref="ISvcKv.BatSet"/> 的資料列。</returns>
+	async Task<PoKv> MkUpsertKv(str Key, PoKv Kv, CT Ct){
+		var OldKv = await SvcKv.GetByOwnerEtKStr(IdUser.Zero, Key, Ct);
+		if(OldKv is not null){
+			Kv.Id = OldKv.Id;
+		}
+		return Kv;
+	}
+
 	public SvcTokenStorage(
 		//ISvcSecretKv SvcSecretKv
 		ISvcKv SvcKv
@@ -35,11 +51,16 @@ public class SvcTokenStorage:ISvcTokenStorage{
 	[Impl]
 	public async Task<nil> SetRefreshToken(str Token, CT Ct){
 		//TODO 先直接存明文 後汶改加密
+		var RefreshTokenKv = await MkUpsertKv(
+			KeysClientKv.RefreshToken+""
+			,new PoKv{
+				Owner = IdUser.Zero,
+			}.SetStrStr(KeysClientKv.RefreshToken, Token)
+			,Ct
+		);
 		await SvcKv.BatSet(
 			null,
-			ToolAsyE.ToAsyE([new PoKv{
-				Owner = IdUser.Zero,
-			}.SetStrStr(KeysClientKv.RefreshToken, Token)])
+			ToolAsyE.ToAsyE([RefreshTokenKv])
 			,Ct
 		);
 		return NIL;
@@ -47,18 +68,33 @@ public class SvcTokenStorage:ISvcTokenStorage{
 
 	[Impl]
 	public async Task<nil> SetRefreshToken(ReqSetRefreshToken Req, CT Ct){
+		var RefreshTokenKv = await MkUpsertKv(
+			KeysClientKv.RefreshToken+""
+			,new PoKv{
+				Owner = IdUser.Zero,
+			}.SetStrStr(KeysClientKv.RefreshToken, Req.RefreshToken)
+			,Ct
+		);
+		var RefreshTokenExpireAtKv = await MkUpsertKv(
+			KeysClientKv.RefreshTokenExpireAt+""
+			,new PoKv{
+				Owner = IdUser.Zero,
+			}.SetStrI64(KeysClientKv.RefreshTokenExpireAt, Req.RefreshTokenExpireAt)
+			,Ct
+		);
+		var CurLoginUserIdKv = await MkUpsertKv(
+			KeysClientKv.CurLoginUserId+""
+			,new PoKv{Owner = IdUser.Zero}
+				.SetStrStr(KeysClientKv.CurLoginUserId, Req.LoginUserId+"")
+			,Ct
+		);
 		await SvcKv.BatSet(
 			null,
 			ToolAsyE.ToAsyE(
 				[
-					new PoKv{
-						Owner = IdUser.Zero,
-					}.SetStrStr(KeysClientKv.RefreshToken, Req.RefreshToken)
-					,new PoKv{
-						Owner = IdUser.Zero,
-					}.SetStrI64(KeysClientKv.RefreshTokenExpireAt, Req.RefreshTokenExpireAt)
-					,new PoKv{Owner = IdUser.Zero}
-					.SetStrStr(KeysClientKv.CurLoginUserId, Req.LoginUserId+"")
+					RefreshTokenKv
+					,RefreshTokenExpireAtKv
+					,CurLoginUserIdKv
 				]
 			)
 			,Ct
