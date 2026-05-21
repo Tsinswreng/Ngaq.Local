bcb9a82d53c284db5933bb86eb62d8c7324efe92
Tsinswreng <Tsinswreng@gmail.com>
2026-05-21 17:14:13 +0800

    chore

--- Domains/Word/Svc/SvcWordV2.Crud.cs
+++ Domains/Word/Svc/SvcWordV2.Crud.cs
@@ -1,36 +1,26 @@
 namespace Ngaq.Backend.Domains.Word.Svc;
 
 using System.Runtime.CompilerServices;
-using Ngaq.Core.Frontend.Kv;
+using Ngaq.Backend.Domains.Word.Dao;
 using Ngaq.Core.Infra;
 using Ngaq.Core.Infra.Errors;
 using Ngaq.Core.Model.Po.Kv;
 using Ngaq.Core.Model.Po.Learn_;
-using Ngaq.Core.Tools;
-using Ngaq.Core.Shared.Sync;
-using Ngaq.Core.Shared.StudyPlan.Models.PreFilter;
-using Ngaq.Core.Shared.StudyPlan.Models.Po.PreFilter;
-using Ngaq.Core.Shared.StudyPlan.Models.Po.StudyPlan;
-using Ngaq.Core.Shared.StudyPlan.Svc;
+using Ngaq.Core.Shared.Base.Models.Po;
+using Ngaq.Core.Shared.Dictionary.Models;
 using Ngaq.Core.Shared.User.Models.Po.User;
 using Ngaq.Core.Shared.Word.Models;
+using Ngaq.Core.Shared.Word.Models.Dto;
 using Ngaq.Core.Shared.Word.Models.Learn_;
 using Ngaq.Core.Shared.Word.Models.Po.Kv;
 using Ngaq.Core.Shared.Word.Models.Po.Learn;
 using Ngaq.Core.Shared.Word.Models.Po.Word;
 using Ngaq.Core.Shared.Word.Svc;
-using Ngaq.Backend.Db.TswG;
+using Ngaq.Core.Tools;
 using Tsinswreng.CsErr;
-using Tsinswreng.CsCore;
 using Tsinswreng.CsSql;
-using Tsinswreng.CsTools;
-using Ngaq.Core.Tools.Json;
-using Ngaq.Core.Shared.Dictionary.Models;
-using Ngaq.Core.Shared.Word.Models.Dto;
-using Ngaq.Core.Shared.Base.Models.Po;
-using Ngaq.Backend.Domains.Word;
-using Ngaq.Backend.Domains.Word.Dao;
 using Tsinswreng.CsTempus;
+using Tsinswreng.CsTools;
 
 public partial class SvcWordV2
 {
